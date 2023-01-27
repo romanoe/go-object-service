cde09ed99855dd1b6080c9652a30af3408bab363
romanoe <noemi.romano@protonmail.com>
2023-01-27 17:36:42 +0100

    ui + ObjectView list

---
+++ ui/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import ObjectsView from "@/components/ObjectsView.vue";
+</script>
+
+<template>
+  <header>
+  </header>
+
+    <Suspense>
+      <objects-view></objects-view>
+    </Suspense>
+
+</template>
+
+<style scoped>
+</style>
---
+++ ui/src/components/ObjectsView.vue
@@ -0,0 +1,33 @@
+<template>
+<h1>Objets</h1>
+  <ul>
+    <li v-for="object in objects">
+      {{object.id}}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts" setup>
+import {ref} from 'vue'
+
+interface GoelandObject {
+  created_at : Date;
+
+  fk_type : number;
+
+  id: number
+}
+
+const objects = ref<GoelandObject[]>([])
+
+async function getData() {
+  const res = await fetch(import.meta.env.VITE_API_URL + '/objects');
+  objects.value = await res.json();
+}
+
+getData();
+</script>
+
+<style scoped>
+
+</style>
