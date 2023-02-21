735aa9eb9cbc310373bdabfc5ef31fc1cfe528dd
sgca0250 <noemi.romano@lausanne.ch>
2023-02-21 10:07:20 +0100

    object list with links to page

--- ui/src/components/ObjectList.vue
+++ ui/src/components/ObjectList.vue
@@ -1,28 +1,25 @@
 <script lang="ts" setup>
 import { useFetch } from "@/composables/FetchData";
-import { ref, defineAsyncComponent } from 'vue';
 
 interface GoelandObject {
   created_at: Date;
   fk_type: number;
-  id: number
+  id: string
 }
+
+  const url = import.meta.env.VITE_API_URL + '/objects'
 
   const {data} = await useFetch<GoelandObject[]>(import.meta.env.VITE_API_URL + '/objects');
 </script>
 
 
 
-  <template>
-
-
+<template>
     <h1>Objets</h1>
     <ul>
-      <li v-for="object in data">
-        {{object.fk_type}}
+        <li v-for="object in data">
+          <a :href="'/' + object.id">{{object.fk_type}}</a>
       </li>
     </ul>
-
-
-  </template>
+</template>
 
